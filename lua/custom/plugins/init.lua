5cfb4bc4d6b3d000d546232757e05ec03247b456
Tadej <tadej.osolnik@gmail.com>
2024-06-07 21:20:47 +0200

    Harpoon settings

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -12,18 +12,42 @@ return {
 
       harpoon:setup()
 
-      vim.keymap.set('n', '<leader>h', function()
-        harpoon:list():add()
-      end, { desc = '' })
+      -- telescope
+      local conf = require('telescope.config').values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require('telescope.pickers')
+          .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+              results = file_paths,
+            },
+            previewer = conf.file_previewer {},
+            sorter = conf.generic_sorter {},
+          })
+          :find()
+      end
+
       vim.keymap.set('n', '<C-e>', function()
-        harpoon.ui.toggle_quick_menu(harpoon:list())
-      end)
-      vim.keymap.set('n', '<C-S-P>', function()
-        harpoon:list():prev()
-      end)
-      vim.keymap.set('n', '<C-S-P>', function()
-        harpoon:list():prev()
-      end)
+        toggle_telescope(harpoon:list())
+      end, { desc = 'Open harpoon window' })
+
+      -- vim.keymap.set('n', '<leader>h', function()
+      --   harpoon:list():add()
+      -- end, { desc = '' })
+      -- vim.keymap.set('n', '<C-e>', function()
+      --   harpoon.ui.toggle_quick_menu(harpoon:list())
+      -- end)
+      -- vim.keymap.set('n', '<C-S-P>', function()
+      --   harpoon:list():prev()
+      -- end)
+      -- vim.keymap.set('n', '<C-S-P>', function()
+      --   harpoon:list():prev()
+      -- end)
     end,
   },
 }
